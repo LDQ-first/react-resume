89aa861425d7764275d739f3566fa9d8adfba8a4
LDQ-first <178848890@qq.com>
2017-09-26 22:56:04 +0800

    'modify'

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,7 +7,7 @@ import {
 } from '../../selector/home.js'
 import HomeDiv from '../../styled/Home.js'
 import Header from '../../components/Header/Header.js'
-
+import classNames from 'classnames'
 
 
 class Home extends Component {
@@ -17,10 +17,53 @@ class Home extends Component {
         }
     }
 
+    constructor (props) {
+        super(props)
+        this.state = {
+            imglists: [
+                '../../../static/img/Header/001.jpg',
+                '../../../static/img/Header/002.jpg',
+                '../../../static/img/Header/003.jpg',
+                '../../../static/img/Header/004.jpg',
+                '../../../static/img/Header/005.jpg',
+                '../../../static/img/Header/006.jpg',
+            ],
+            imgIndex: 0
+        }
+    }
+
+    componentWillMount() {
+        this._showBgImg()
+    }
+
+    _showBgImg() {
+        const {imglists} = this.state
+        let i = 0
+        setInterval(() => {
+            i = i < imglists.length - 1 ? i + 1 : 0 
+            this.setState({
+                imgIndex: i
+            })
+        }, 10000)
+    }
+
     render() {
+
+        const {imglists, imgIndex} = this.state
+
+        const Imglists = imglists.map((imglist, index) => {
+            return (
+                <img key={index} src={imglist} className={classNames('imglist', {hide: index === imgIndex})}/>
+            )
+        })
+
         
         return (
             <HomeDiv>
+
+                <div className="bgImg-list">
+                    {Imglists}
+                </div>
                 <div className="home">
                     home
                 </div>
