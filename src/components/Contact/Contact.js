e00ade10d7cc1eea17958d43c880fb215f6be80e
LDQ-first <178848890@qq.com>
2017-09-28 15:26:09 +0800

    'modify'

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import  ContactDiv  from '../../styled/Contact.js'
 
+
+
 export default class Contact extends Component {
     static get propTypes() {
         return {
            _this: PropTypes.object
         }
     }
+
+      constructor(props) {
+        super(props) 
+        this.state = {
+            contactArr: [
+                {   
+                    name: '手机',
+                    icon: 'shouji',
+                    content: '18826136763'
+                },
+                {
+                    name: '微信',
+                    icon: 'wechat-copy',
+                    content: '18826136763'
+                },
+                {
+                    name: '163邮箱',
+                    icon: 'youxiang',
+                    content: '18826136763@163.com'
+                },
+                {
+                    name: 'QQ邮箱',
+                    icon: 'youxiang',
+                    content: '2320975287@qq.com'
+                }
+            ]
+        }
+    } 
 
      componentDidMount() {
          const {_this} = this.props
@@ -18,13 +48,58 @@ export default class Contact extends Component {
     
 
     render() {
+
+         const { contactArr } = this.state
+
+
+          let contacts = contactArr.map((contact, index) => {
+
+            let contents = null
+            if(contact.content instanceof Array) {
+                 contents = contact.content.map((content, index) => {
+                     return (
+                         <p key={index} className="content">
+                            {content}
+                         </p>
+                     )
+                 })
+            }
+           
+
+
+            return (
+                <li key={index} className="contact">
+                    <div className="title">
+                        <svg className="icon" aria-hidden="true">
+                            <use xlinkHref={`#icon-${contact.icon}`}></use>
+                        </svg>
+                        <span className="name">{contact.name}</span>
+                    </div>
+                    { 
+                        contents 
+                        ?  <div className="contentWrap">
+                            {contents}
+                           </div> 
+                        :  <p className="content">{contact.content}</p>
+                    }
+                </li>
+            )
+        })
     
         return (
             <ContactDiv>
                <div className="contactContent" ref={contact => this._contact = contact}>
-                   <h2 className="contact">
+                   <h2 className="title">
                     联系方式
                   </h2>
+                  <div className="contactWrap">
+                      <div className="centerArea">
+                          centerArea
+                      </div>
+                      <ul className="contacts">
+                          { contacts }
+                      </ul>
+                  </div>
                </div>
             </ContactDiv>
         )
--- src/styled/Contact.js
+++ src/styled/Contact.js
@@ -2,11 +2,20 @@ import styled from 'styled-components'
 
 const ContactDiv = styled.div`
      padding: 4em 0;
+     background: #FFF;
     .contactContent {
         max-width: 900px;
         margin: 0 auto;
-        .contact {
+        .title {
             text-align: center;
+            color: #6C73C4;
+            margin-bottom: 2em;
+        }
+        .contactWrap {
+            color: #6C73C4;
+            .contact {
+
+            }
         }
     }
     
