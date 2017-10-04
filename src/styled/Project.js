adcff5d4fd591b097b24f0255e152095cc566950
LDQ-first <178848890@qq.com>
2017-10-05 00:19:58 +0800

    'modify'

--- src/styled/Project.js
+++ src/styled/Project.js
@@ -149,7 +149,7 @@ const ProjectDiv = styled.div`
                         left: 0;
                         width: 100%;
                         height: 100%;
-                        background: rgba(0, 0, 0, 0.4);
+                        background: rgba(0, 0, 0, 0.2);
                         opacity: 0;
                         transition: all 0.3s ease;
                         &::before {
